9390297407ece711703de88d2da9c0b47011fef0
bill611 <bill4regist@163.com>
2019-12-22 00:48:53 +0800

    修改监视脚本，支持sd卡升级

--- root_usr/monitor.sh
+++ root_usr/monitor.sh
@@ -2,5 +2,19 @@
 
 /root/usr/cat_eye
 
+if [ $? -ne 1 ] 
+then
+	if [ -d "/mnt/sdcard/update" ] ;then
+        cd /mnt/sdcard/update
+        if [ -e "go.sh" ] ;then
+            ./go.sh
+        else
+            echo "the go.sh is not exist !!!,pleas download the new update file again!"
+        fi
+        rm -rf update
+    fi
+fi
+
+sync
 
 reboot
